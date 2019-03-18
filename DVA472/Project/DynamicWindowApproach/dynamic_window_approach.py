c441908649f74800bda09f2988d5838c1f915007
Najda <nvc15001@student.mdh.se>
2019-03-18 17:09:40 +0100

    new dwa

--- DVA472/Project/DynamicWindowApproach/dynamic_window_approach.py
+++ DVA472/Project/DynamicWindowApproach/dynamic_window_approach.py
@@ -24,42 +24,80 @@ class Config():
 
     def __init__(self):
         # robot parameter
-        self.max_speed = 1.0  # [m/s]
+        self.max_speed = 0.5  # [m/s] 0.02
         self.min_speed = 0   # [m/s]
-        self.max_yawrate = 30.0 * math.pi / 180.0  # [rad/s]
+        self.max_yawrate = 10.0 * math.pi / 180.0  # [rad/s] -good value
         self.max_accel = 0.2  # [m/ss]
-        self.max_dyawrate = 30.0 * math.pi / 180.0  # [rad/ss]
+        self.max_dyawrate = 150.0 * math.pi / 180.0  # [rad/ss]
         self.v_reso = 0.01  # [m/s]
-        self.yawrate_reso = 0.1 * math.pi / 180.0  # [rad/s]
+        self.yawrate_reso = 0.1 * math.pi / 180.0  # [rad/s] 
         self.dt = 0.1  # [s]
         self.predict_time = 3.0  # [s]
         self.to_goal_cost_gain = 1.0
         self.speed_cost_gain = 1.0
-        self.robot_radius = 0.3  # car radius(?) + obstacle radius(0.10) [m] = 0.3
-        self.x_comp=208/10
-        self.y_invert=416/10
-        self.y=80/10
-    
-
-#ymin, ymax, xmin, xmax
+        self.robot_radius = 0.4  # car radius(?) + obstacle radius(0.10) [m] = 0.3
+        self.x_comp=1
+        self.y_invert=416
+        self.y=1
 def extract_obs(j,boxes,config):
+    x = boxes[j]
+    y = boxes[j+1]
+    X1,Y1 = np.meshgrid(x,y)
+    ob = np.array([X1.flatten(), Y1.flatten()]).T
+    return ob
+#ymin, ymax, xmin, xmax
+def extract_lines(j,boxes,config):
 
+    retur=2
+    # Extract corners of detected lines and obstacles
     ymin=config.y_invert-boxes[j]
     ymax=config.y_invert-boxes[j+1]
+    #xmin=boxes[j+2]-config.x_comp
+    #xmax=boxes[j+3]-config.x_comp
+
     xmin=boxes[j+2]-config.x_comp
     xmax=boxes[j+3]-config.x_comp
 
-    x = (xmax + xmin)/2
+    y = [192,205,248]
+    y = np.sort(y)
+    dist = [1,2,3]
+    ymin = np.interp(ymin, y, dist)
+    ymax = np.interp(ymax, y, dist)
+
+    # Calculate middle
     y = (ymax + ymin)/2
-      
+
+    pixel_xmin = y * 0.75
+    pixel_v_min = pixel_xmin/416
+    xmin = xmin*pixel_v_min
+    xmin = xmin - (y*0.75)
+
+    pixel_xmax = y * 0.75
+    pixel_v_max =pixel_xmax/416
+    xmax = xmax*pixel_v_max
+    xmax = xmax - (y*0.75)
+
+
+    # Calculate middle
+    x = (xmax + xmin)/2
+    print("Calculated x",x)
+    
+
+    # Check if line can be determined with confidence 
+    if x>0.25:
+        retur=0 # right
+    elif x<-0.25:
+        retur=1 # left
+
     X1,Y1 = np.meshgrid(x,y)
     ob = np.array([X1.flatten(), Y1.flatten()]).T
         
-    return ob
+    return ob, retur
     
 
 def motion(x, u, dt):
     # motion model
+    # initial state [x(m), y(m), yaw(rad), v(m/s), omega(rad/s)]
 
     x[2] += u[1] * dt
     x[0] += u[0] * math.cos(x[2]) * dt
@@ -74,7 +112,7 @@ def calc_dynamic_window(x, config):
 
     # Dynamic window from robot specification
     Vs = [config.min_speed, config.max_speed,
-          -config.max_yawrate, config.max_yawrate]
+    -config.max_yawrate, config.max_yawrate]
 
     # Dynamic window from motion model
     Vd = [x[3] - config.max_accel * config.dt,
@@ -113,6 +151,7 @@ def calc_final_input(x, u, dw, config, goal, ob):
     # evalucate all trajectory with sampled input in dynamic window
     for v in np.arange(dw[0], dw[1], config.v_reso):
         for y in np.arange(dw[2], dw[3], config.yawrate_reso):
+            
             traj = calc_trajectory(xinit, v, y, config)
 
             # calc cost
@@ -125,13 +164,12 @@ def calc_final_input(x, u, dw, config, goal, ob):
             final_cost = to_goal_cost + speed_cost + ob_cost 
 
             #print (final_cost)
-
             # search minimum trajectory
             if min_cost >= final_cost:
                 min_cost = final_cost
                 min_u = [v, y]
                 best_traj = traj
-
+    #print("y:",y)
     return min_u, best_traj
 
 
@@ -140,10 +178,9 @@ def calc_obstacle_cost(traj, ob, config):
 
     skip_n = 2
     minr = float("inf")
-
     for ii in range(0, len(traj[:, 1]), skip_n):
         for i in range(len(ob[:, 0])):
-            
+                
             ox = ob[i, 0]
             oy = ob[i, 1]      
             dx = traj[ii, 0] - ox 
@@ -188,13 +225,11 @@ def plot_arrow(x, y, yaw, length=0.5, width=0.1):  # pragma: no cover
     plt.plot(x, y)
 
 
-def main(gx, gy, right_lines, left_lines, boxes): #gx=0.0, gy=5.0
-    y_increment = 5.0
-    x_temp_inc = 0.0
+def main(gx, gy, d_lines, boxes,prev_angle): #gx=0.0, gy=5.0
     config = Config()
     print(__file__ + " start!!")
     # initial state [x(m), y(m), yaw(rad), v(m/s), omega(rad/s)]
-    x = np.array([0, 0, math.pi / 8.0, 0.0, 0.0])
+    x = np.array([0, 0, 90*(math.pi / 180.0), 0.0, 0.0])
     # goal position [x(m), y(m)]
     ref = np.array([0,config.x_comp])
     goal = np.array([gx, gy])
@@ -202,57 +237,84 @@ def main(gx, gy, right_lines, left_lines, boxes): #gx=0.0, gy=5.0
     
     traj = np.array(x)
 
+    
+
     ob = []
     ob = np.array(ob)
     j = 1
+    retur = 3
     for i in range(0, boxes[0]):
         temp_ob=extract_obs(j,boxes,config)
-        j = j+5
+        j = j+2
         if i !=0:
             ob = np.concatenate((ob,temp_ob), axis = 0)
         else:
             ob = temp_ob
 
+    retur = 3
     right_L = []
     right_L = np.array(right_L)
-    j = 1
-    for i in range(0, right_lines[0]):
-        temp_right=extract_obs(j,right_lines,config)
-        j = j+5
-        if i !=0:
-            right_L = np.concatenate((right_L,temp_right), axis = 0)
-        else:
-            right_L = temp_right
-
     left_L = []
     left_L = np.array(left_L)
     j = 1
-    for i in range(0, left_lines[0]):
-        temp_left=extract_obs(j,left_lines,config)
-        j = j+5
-        if i !=0:
-            left_L = np.concatenate((left_L,temp_left), axis = 0)
-        else:
-            left_L = temp_left
+    add_l=0
+    add_r=0
+    for i in range(0, d_lines[0]):
+
+        temp_line, retur = extract_lines(j,d_lines,config)
+        j = j+4
+        if retur == 0:
+            if add_r != 0:
+                right_L = np.concatenate((right_L,temp_line), axis = 0)
+            else:
+                right_L = temp_line
+            add_r+= 1
+        elif retur == 1:
+            if add_l !=0:
+                left_L = np.concatenate((left_L,temp_line), axis = 0) # CHANGE CONCATENATE, i!=0 galler ej
+            else:
+                left_L = temp_line
+            add_l+= 1
+        elif retur == 2:
+            if prev_angle < 0:
+                if add_l !=0:
+                    left_L = np.concatenate((left_L,temp_line), axis = 0) 
+                else:
+                    left_L = temp_line
+            else:
+                if add_r != 0:
+                    right_L = np.concatenate((right_L,temp_line), axis = 0)
+                else:
+                    right_L = temp_line
+                add_r+= 1
+
     
-    #while(1):
-    for i in range(35):
+        
+    for i in range(1000):# change to a lower loop-value
         u, ltraj = dwa_control(x, u, config, goal, ob)
 
         x = motion(x, u, config.dt)
         traj = np.vstack((traj, x))  # store state history
+
+        left_L_sum = [[0.0,0.0]]
+        left_L_sum = np.array(left_L_sum)
+
+        right_L_sum = [[0.0,0.0]]
+        right_L_sum = np.array(right_L_sum)
 
         if show_animation:
             plt.cla()
             plt.plot(ltraj[:, 0], ltraj[:, 1], "-g")
             plt.plot(x[0], x[1], "xr")
             plt.plot(goal[0], goal[1], "xb")
-            plt.plot(right_L[0][0], right_L[0][1], "ok")
-            plt.plot(left_L[0][0], left_L[0][1], "ok")
-            plt.plot(ref[0], ref[1], "ok")
-            plt.plot(ob[:, 0], ob[:, 1], "ok")
+            
+            plt.plot(right_L_sum[0][0], right_L_sum[0][1], "or")
+            
+            plt.plot(left_L_sum[0][0], left_L_sum[0][1], "om")
+            plt.plot(ref[0], ref[1], "og")
+            plt.plot(ob[:, 0], ob[:, 1], "ob")
             plot_arrow(x[0], x[1], x[2])
-            plt.axis([-config.x_comp,config.x_comp,0,config.y_invert])
+            plt.axis([-3.75,3.75,0,5])
             plt.grid(True)
             plt.pause(0.0001)
 
@@ -262,30 +324,101 @@ def main(gx, gy, right_lines, left_lines, boxes): #gx=0.0, gy=5.0
             break
 
         #Magnitude right line
-        if right_lines[0] != 0:
+        mag_right = 0
+        mag_r_temp = 0
+        
+        if len(right_L)!= 0 and add_r!=0:
+            for i in range(0,add_r):
+                mag_r_temp= math.sqrt((right_L[i][0] - ref[0])**2 + (right_L[i][1] - ref[1])**2)
+                mag_right = mag_right + mag_r_temp
+                right_L_sum[0][0] = right_L[i][0] + right_L_sum[0][0]
+                right_L_sum[0][1] = right_L[i][1] + right_L_sum[0][1]
+            mag_right = mag_right/len(right_L)
+            right_L_sum = right_L_sum/len(right_L)
+        else:
+            temp_r_l = [[0.75, 5]]
+            temp_r_l = np.array(temp_r_l)
+            right_L = temp_r_l
             mag_right = math.sqrt((right_L[0][0] - ref[0])**2 + (right_L[0][1] - ref[1])**2)
-        else:
-            mag_right = 1000
-
+            right_L_sum = right_L
+            
+        mag_left = 0
+        mag_l_temp = 0
+        
         #Magnitude left line
-        if left_lines[0] != 0:
-            mag_left = math.sqrt((left_L[0][0] - ref[0])**2 + (left_L[0][1] - ref[1])**2)
+        print("addL", add_l)
+        print("addr", add_r)
+        if len(left_L) != 0 and add_l!=0:
+            for i in range(0,add_l):
+                mag_l_temp= math.sqrt((left_L[i][0] - ref[0])**2 + (left_L[i][1] - ref[1])**2)
+                mag_left = mag_left + mag_l_temp
+                left_L_sum[0][0] = left_L[i][0] + left_L_sum[0][0]
+                left_L_sum[0][1] = left_L[i][1] + left_L_sum[0][1]
+            mag_left=mag_left/len(left_L)
+            left_L_sum = left_L_sum/len(left_L)
         else:
-            mag_left = 1000
+            temp_l_l = [[-0.75, 5]]
+            temp_l_l = np.array(temp_l_l)
+            left_L = temp_l_l
+            mag_left = math.sqrt((left_L[0][0] - ref[0])**2 + (left_L[0][1] - ref[1])**2)
+            left_L_sum = left_L
 
+        print("left sum:",left_L_sum)
+        print("right sum:", right_L_sum)
+        print("left mag:",mag_left)
+        print("right mag:", mag_right)
+        #Calculate goal based on magnitude
+        mag_tot=0
         mag_tot = (mag_right + mag_left) /2
-        x_cord_r = right_L[0][0] - math.sqrt(mag_tot**2 - (right_L[0][1]- (ref[1]+config.y))**2)
-        x_cord_l = left_L[0][0] - math.sqrt(mag_tot**2 - (left_L[0][1]- (ref[1]+config.y))**2)
-        goal[0] = round(((x_cord_r+config.x_comp) + (x_cord_l+config.x_comp))/2,1)
+        x_cord_r=0
+        x_cord_l=0
+        x_cord_r = right_L_sum[0][0] - math.sqrt(mag_tot**2 - (right_L_sum[0][1]- (ref[1]+config.y))**2)
+        x_cord_l = left_L_sum[0][0] - math.sqrt(mag_tot**2 - (left_L_sum[0][1]- (ref[1]+config.y))**2)
+        print("x-cord r:",x_cord_r)
+        print("x-cord l:",x_cord_l)
+        goal[0] = (x_cord_r + x_cord_l)/2
         goal[1] = ref[1] + config.y
-        print("vinkel:",x[2])
-        print("l-traj:", ltraj) 
-    print("Done")
+        print("goal", goal)
+
+        # Previous implementation of reallocation - do we still need it?
+        """
+        angle = math.degrees(math.acos(math.cos(x[2]))) #- 90 # postive angle -> left turn
+        print("calc angle:", angle)
+        if boxes[0] == 0:
+            goal[0] = round(((x_cord_r+config.x_comp) + (x_cord_l+config.x_comp))/2,1)
+        else:
+            m=0
+            for l in range(0,boxes[0]):
+                if angle == math.degrees(math.atan2(ob[m][m+1]-x[1],ob[m][m]-x[0])):
+                    if goal[0] > ob[m][m]:
+                        goal[0] = round(((x_cord_r+config.x_comp) + (x_cord_l+config.x_comp))/2,1) + 2
+                        m+2
+                    else:
+                        goal[0] = round(((x_cord_r+config.x_comp) + (x_cord_l+config.x_comp))/2,1) - 2
+                        m+2
+
+        goal[1] = ref[1] + config.y
+
+        if angle >= 30:
+            angle = 30
+        elif angle <= -30:
+            angle = -30
+        print("angle:", math.degrees(x[2]))
+    
+        """
+        # Only test - placing goal on obstacle 
+        #ob[0][0] = goal[0] 
+        #ob[0][1] = goal[1] 
+
+        # Calculate steering angle based on trajectory
+        line_angle = math.degrees(ltraj[len(ltraj)-1][2])
+        angle_temp=line_angle - math.degrees(x[2])
+
     if show_animation:
         plt.plot(traj[:, 0], traj[:, 1], "-r")
         plt.pause(0.0001)
-
-    plt.show()
+    plt.show()     
+    return angle_temp
 
 
 #if __name__ == '__main__':
--- DVA472/Project/DynamicWindowApproach/main.py
+++ DVA472/Project/DynamicWindowApproach/main.py
@@ -1,25 +1,33 @@
 #import importlib
 #importlib.import_module(dynamic_window_approach.py)
+import numpy as np
 import dynamic_window_approach as DWA
+#from scipy import interpolate
 #dwa = DWA()
 class obstacle:
     def __init__(self):
-        right_lines = [] # Number of right lines, ymin, ymax, xmin, xmax, distance
-        left_lines = [] # Number of left lines, ymin, ymax, xmin, xmax, distance
-        boxes = [] # Number of boxes, ymin, ymax, xmin, xmax, distance
-        prevgoal = []
+        self.lines = [] # Number of right lines, ymin, ymax, xmin, xmax
+        self.boxes = [] # Number of boxes, ymin, ymax, xmin, xmax, distance
+        self.gx = 0.0
+        self.gy = 5.0
         
 def main():
     obs=obstacle()
-    gx = 0.0
-    gy = 5.0
+    angle = 0.0
     while(True):
         #yolo..() collect obstacles in class variables
-        obs.right_lines = [1,0.4,1,24.0,30.0,1.0]
-        obs.left_lines = [1,0.4,3.3,0.2,0.8,1.0]
-        obs.boxes = [2,15.01,16.39,0.05,0.60,10,15.01,16.39,0.05,0.60,14]
+        obs.lines = [1,10,1,280,320]
+        obs.boxes = [1, 1,2] #[1,15.01,16.39,0.05,0.60,14] check if zero from yolo
+        y = [192,205,248]
+        y = np.sort(y)
+        dist = [1,2,3]
+        inter = np.interp(204, y, dist)
+        print("interp:",inter)
 
-        DWA.main(gx, gy, obs.right_lines, obs.left_lines, obs.boxes)
+
+        angle = DWA.main(obs.gx, obs.gy, obs.lines, obs.boxes, angle)
+
+
 
 if __name__ == '__main__':
     main()
