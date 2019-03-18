6d863712d0f350bc9dbe92fedb781b19682568d8
Jonathan Thörn <jonathan.thoern@gmail.com>
2019-03-18 10:59:42 +0100

    Update testMain

--- DVA472/Project/YOLO_training/testMain.py
+++ DVA472/Project/YOLO_training/testMain.py
@@ -9,12 +9,12 @@ yolo = YOLO()
 class obstacle:
     BOX_WIDTH = float(20.0) # Expressed in cm
     FOCAL_LENGHT = float(18) # Expressed in cm
-
-    right_lines = [] # num of right lines, y_min, y_max, x_min, x_max - add magnitude(i.e distance)???
-    left_lines = [] # num of left lines, y_min, y_max, x_min, x_max - add magnitude(i.e distance)???
+    DIST_MIN = 30 # Used when box is partially outside of frame
+    TWO_BOX_RATIO = 3.5
+    lines = [] # num of lines, y_min, y_max, x_min, x_max
     boxes = [] # num of boxes, y_min, y_max, x_min, x_max, distance(in cm!)
 
-    '''
+    ''' Is not needed 
     def __init__(self):
         self.right_lines = [] # num of right lines, y_min, y_max, x_min, x_max
         self.left_lines = [] # num of left lines, y_min, y_max, x_min, x_max
@@ -23,11 +23,9 @@ class obstacle:
     
     def updateCoordinates(self, detected, scores, classes):
 
-        temp_right = [] # num, y_min, y_max, x_min, x_max
-        temp_left = [] # num, y_min, y_max, x_min, x_max
+        temp_lines = [] # num, y_min, y_max, x_min, x_max, score
         temp_boxes = [] # num, y_min, y_max, x_min, x_max
-        num_right = 0
-        num_left = 0
+        num_lines = 0
         num_boxes = 0
         y_min = 0
         y_max = 0
@@ -37,60 +35,75 @@ class obstacle:
         for i, c in reversed(list(enumerate(classes))):
             #predicted_class = self.class_names[c]
             box = detected[i]
-            score = scores[i]
+            # score = scores[i] Not used, but could be
             y_min, x_min, y_max, x_max = box  #top, left, bottom, right = box
             y_min = max(0, np.floor(y_min + 0.5).astype('int32'))
             x_min = max(0, np.floor(x_min + 0.5).astype('int32'))
             y_max = min(416, np.floor(y_max + 0.5).astype('int32'))
             x_max = min(416, np.floor(x_max + 0.5).astype('int32'))
 
-            if(classes[i] == 0):
-                num_right = num_right + 1
-                temp_right.extend((y_min, y_max, x_min, x_max))
-            
-            if(classes[i] == 1):
-                num_left = num_left + 1
-                temp_left.extend((y_min, y_max, x_min, x_max))
+            if(classes[i] == 0 or classes[i]  == 1): # Line (either R or L)
+                
+                num_lines = num_lines + 1
+                temp_lines.extend((y_min, y_max, x_min, x_max))
 
-            if(classes[i] == 2):
-                num_boxes = num_boxes + 1
-                temp_boxes.extend((y_min, y_max, x_min, x_max))
-                width = x_max - x_min
-                box_dist = self.getBoxDistance(width)
-                temp_boxes.append(box_dist)
-        
-        temp_right.insert(0, num_right) # Add number of, in first position
-        temp_left.insert(0, num_left)
+            if(classes[i] == 2): # Obstacle
+
+                boxW = x_max - x_min
+                boxH = y_max - y_min
+                boxRatio = boxW / boxH
+
+                if(boxRatio > self.TWO_BOX_RATIO):
+                    divider = x_min + (boxW / 2)
+                    box1_xmin = x_min
+                    box1_xmax = divider - 1
+                    box2_xmin = divider + 1
+                    box2_xmax = x_max
+                    if(x_min > 0 and x_max < 416): #Dont calculate on boxes out of frame
+                        box1_dist = self.getBoxDistance(box1_xmax-box1_xmin)
+                        box2_dist = self.getBoxDistance(box2_xmax-box2_xmin)
+                    else:
+                        box1_dist = self.DIST_MIN # Set fixed value when box partially out of frame
+                        box2_dist = self.DIST_MIN
+                    num_boxes = num_boxes + 2
+                    temp_boxes.extend((y_min, y_max, box1_xmin, box1_xmax, box1_dist))
+                    temp_boxes.extend((y_min, y_max, box2_xmin, box2_xmax, box2_dist))
+                else:
+                    num_boxes = num_boxes + 1
+                    temp_boxes.extend((y_min, y_max, x_min, x_max))
+                    width = x_max - x_min
+                    if(x_min > 0 and x_max < 416): #Dont calculate on boxes out of frame
+                        box_dist = self.getBoxDistance(width)
+                        # Distance from IR and Kalman could be applied here before appending to array
+                    else:
+                        box_dist = self.DIST_MIN # Set fixed value when box partially out of frame
+                    temp_boxes.append(box_dist)
+
+        temp_lines.insert(0, num_lines) # Add number of, in first position
         temp_boxes.insert(0, num_boxes)
 
-        self.right_lines = temp_right
-        self.left_lines = temp_left
+        self.lines = temp_lines
         self.boxes = temp_boxes
-
 
     def getBoxDistance(self, width):
         
-        #print("width: ",width)
         width = width * 0.0264583333
         distance = (self.BOX_WIDTH * self.FOCAL_LENGHT) / width
         distance = int(round(distance, 0))
         return distance # returns distance to obstacle in cm
-
-
+            
 def main():
     obs = obstacle()
     while True:
         ret,img=cam.read()
         detected, scores, classes = yolo.detect_image(img)
-        #print("Detected: ", detected)
-        #print("classes: ", classes)
         obs.updateCoordinates(detected, scores, classes)
 
+        # cv2 rectangle could be used here to show detected graphically
         cv2.namedWindow("Frame", cv2.WINDOW_NORMAL)
         cv2.imshow("Frame", img)
 
-        print("Right: ", obs.right_lines)
-        print("Left: ", obs.left_lines)
+        print("Lines: ", obs.lines)
         print("Box: ", obs.boxes)
 
         # Close with q
